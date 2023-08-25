1717eb1002e8367eb0a8ca2058c15ba05db831c7
Moin Vinchhi <moin.v@ahduni.edu.in>
2023-08-25 12:19:08 +0530

    added assignment

---
+++ assignment/23.08.2023/485.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+
+        int count=0;
+        int c = 0;
+
+        for (int i=0; i<nums.size(); i++){
+
+            if (nums[i]==1){
+                count++;
+                if (count > c){
+                    c=count;
+                }
+            }
+            else {
+                count=0;
+            }
+        }
+        return c;
+    }
+};
